6ab997298481cde6b077ec230e5f2b21724dfbeb
Terence Wu <terenceccwu@gmail.com>
2017-06-27 09:13:58 +0800

    add youtube stream link fetching

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express')
+const fs = require('fs');
+const ytdl = require('ytdl-core');
+
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.header('Access-Control-Allow-Origin', '*');
+
+  const url = "https://www.youtube.com/watch?v=" + req.query.v
+
+  const opts = {filter: function(format) { return !format.bitrate && format.audioBitrate && format.audioEncoding == 'aac'; }}
+
+  ytdl.getInfo(url)
+    .then(info => ytdl.chooseFormat(info.formats,opts))
+    .then(format => res.json(format))
 })
 
 app.listen(3000, function () {
