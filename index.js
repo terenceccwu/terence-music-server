dcb9e0140f263b30a02890632b25d3747a968062
Terence Wu <terenceccwu@gmail.com>
2017-06-29 02:31:00 +0800

    add fake cache

--- index.js
+++ index.js
@@ -4,16 +4,25 @@ const ytdl = require('ytdl-core');
 
 const app = express()
 
+var cache = {}
+
 app.get('/', function (req, res) {
   res.header('Access-Control-Allow-Origin', '*');
 
-  const url = "https://www.youtube.com/watch?v=" + req.query.v
+  const vid = req.query.v
+  const url = "https://www.youtube.com/watch?v=" + vid
 
-  const opts = {filter: function(format) { return !format.bitrate && format.audioBitrate && format.audioEncoding == 'aac'; }}
-
-  ytdl.getInfo(url)
+  if(cache[vid] && cache[vid].expire > Date.now() ) {
+    res.redirect(cache[vid].url)
+  } else {
+    const opts = {filter: function(format) { return !format.bitrate && format.audioBitrate && format.audioEncoding == 'aac'; }}
+    ytdl.getInfo(url)
     .then(info => ytdl.chooseFormat(info.formats,opts))
-    .then(format => res.json(format))
+    .then(format => {
+      cache[vid] = {url: format.url, expire: Date.now() + 1000 * 10} // expire in 3hr
+      res.redirect(cache[vid].url)
+    })
+  }
 })
 
 app.listen(3000, function () {
