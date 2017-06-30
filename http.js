f5a0693e54216a6ce4275f77479f25f8f98561e4
Terence Wu <terenceccwu@gmail.com>
2017-06-30 09:11:19 +0800

    update

---
+++ http.js
@@ -0,0 +1,48 @@
+
+var fs = require("fs"),
+    http = require("http"),
+    url = require("url"),
+    path = require("path");
+
+http.createServer(function (req, res) {
+  if (req.url != "/output") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end('<audio src="http://localhost:8888/output" controls></audio>');
+  } else {
+    console.log("in");
+    var file = path.resolve(__dirname,"output");
+    fs.stat(file, function(err, stats) {
+      if (err) {
+        if (err.code === 'ENOENT') {
+          // 404 Error if file not found
+          return res.sendStatus(404);
+        }
+      res.end(err);
+      }
+      var range = req.headers.range;
+      if (!range) {
+       // 416 Wrong range
+       return res.sendStatus(416);
+      }
+      var positions = range.replace(/bytes=/, "").split("-");
+      var start = parseInt(positions[0], 10);
+      var total = stats.size;
+      var end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+      var chunksize = (end - start) + 1;
+
+      res.writeHead(206, {
+        "Content-Range": "bytes " + start + "-" + end + "/" + total,
+        "Accept-Ranges": "bytes",
+        "Content-Length": chunksize,
+        "Content-Type": "audio/ogg"
+      });
+
+      var stream = fs.createReadStream(file, { start: start, end: end })
+        .on("open", function() {
+          stream.pipe(res);
+        }).on("error", function(err) {
+          res.end(err);
+        });
+    });
+  }
+}).listen(8888);
--- index.js
+++ index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const fs = require('fs');
 const ytdl = require('ytdl-core');
 
+const logger = require('morgan')
 const app = express()
+
+app.use(logger('dev'))
 
 var cache = {}
 
@@ -12,17 +15,57 @@ app.get('/', function (req, res) {
   const vid = req.query.v
   const url = "https://www.youtube.com/watch?v=" + vid
 
-  if(cache[vid] && cache[vid].expire > Date.now() ) {
-    res.redirect(cache[vid].url)
+  if (req.headers['range']) {
+    var range = req.headers.range;
+    var parts = range.replace(/bytes=/, "").split("-");
+    var partialstart = parts[0];
+    var partialend = parts[1];
+
+    var start = parseInt(partialstart, 10);
+    var end = partialend ? parseInt(partialend, 10) : null;
+
+    const opts = {range: {start: start, end: end}, filter: function(format) { return !format.bitrate && format.audioBitrate && format.container != 'webm'; }}
+    var stream = ytdl(url, opts)
+      .on('info', function (info, format) {
+        var total = format.clen
+        if(!end) end = total - 1
+        var chunksize = (end-start)+1;
+        console.log('RANGE: ' + start + ' - ' + end + ' = ' + chunksize);
+        res.writeHead(206, { 'Content-Range': 'bytes ' + start + '-' + end + '/' + total, 'Accept-Ranges': 'none', 'Content-Length': chunksize, 'Content-Type': format.type });
+      })
+      stream.pipe(res);
   } else {
     const opts = {filter: function(format) { return !format.bitrate && format.audioBitrate && format.audioEncoding == 'aac'; }}
-    ytdl.getInfo(url)
-    .then(info => ytdl.chooseFormat(info.formats,opts))
-    .then(format => {
-      cache[vid] = {url: format.url, expire: Date.now() + 1000 * 10} // expire in 3hr
-      res.redirect(cache[vid].url)
-    })
+    var stream = ytdl(url, opts)
+      .on('info', function (info, format) {
+        var total = format.clen
+        console.log('ALL: ' + total);
+        res.writeHead(200, { 'Content-Length': format.clen, 'Content-Type': format.type });
+      })
+    stream.pipe(res)
   }
+ //  var path = 'test.m4a';
+ // var stat = fs.statSync(path);
+ // var total = stat.size;
+ // if (req.headers['range']) {
+ //   var range = req.headers.range;
+ //   var parts = range.replace(/bytes=/, "").split("-");
+ //   var partialstart = parts[0];
+ //   var partialend = parts[1];
+ //
+ //   var start = parseInt(partialstart, 10);
+ //   var end = partialend ? parseInt(partialend, 10) : total-1;
+ //   var chunksize = (end-start)+1;
+ //   console.log('RANGE: ' + start + ' - ' + end + ' = ' + chunksize);
+ //
+ //   var file = fs.createReadStream(path, {start: start, end: end});
+ //   res.writeHead(206, { 'Content-Range': 'bytes ' + start + '-' + end + '/' + total, 'Accept-Ranges': 'bytes', 'Content-Length': chunksize, 'Content-Type': 'video/mp4' });
+ //   file.pipe(res);
+ // } else {
+ //   console.log('ALL: ' + total);
+ //   res.writeHead(200, { 'Content-Length': total, 'Content-Type': 'video/mp4' });
+ //   fs.createReadStream(path).pipe(res);
+ // }
 })
 
 app.listen(3000, function () {
